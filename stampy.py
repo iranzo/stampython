954fc448a12aba55b7b6ca2b488db6d9f912acdf
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2015-06-25 22:44:48 +0200

    Provide support for database storing of values

--- stampy.py
+++ stampy.py
@@ -25,7 +25,7 @@
 import optparse
 import json
 import urllib
-# import sqlite3 as lite
+import sqlite3 as lite
 import sys
 
 description = """
@@ -42,6 +42,7 @@ p.add_option('-v', "--verbosity", dest="verbosity", help="Show messages while ru
              type='int')
 p.add_option('-u', "--url", dest="url", help="Define URL for accessing bot API", default="https://api.telegram.org/bot")
 p.add_option('-c', '--control', dest='control', help="Define chat_id for monitoring service", default=None)
+p.add_option('-d', '--daemon', dest='daemon', help="Run as daemon", default=None)
 
 (options, args) = p.parse_args()
 
@@ -51,8 +52,6 @@ if options.token:
 else:
     print "Token required for operation, please check https://core.telegram.org/bots"
     sys.exit(1)
-
-con = None
 
 
 def sendmessage(options, chat_id=0, text="", reply_to_message_id=None):
@@ -67,8 +66,8 @@ def getupdates(options, offset=0, limit=100):
     url = "%s%s/getUpdates" % (options.url, options.token)
     message = "%s?" % url
     if offset != 0:
-        message = message + "&offset=%s" % offset
-    message = message + "&limit=%s" % limit
+        message = message + "offset=%s&" % offset
+    message = message + "limit=%s" % limit
     result = None
     try:
         result = json.load(urllib.urlopen(message))
@@ -78,7 +77,71 @@ def getupdates(options, offset=0, limit=100):
         yield item
 
 
+def clearupdates(options, offset):
+    url = "%s%s/getUpdates" % (options.url, options.token)
+    message = "%s?" % url
+    message = message + "offset=%s&" % offset
+    try:
+        result = json.load(urllib.urlopen(message))
+    except:
+        result = None
+    return result
+
+
+def updatekarma(options, word=None, change=0):
+    value = getkarma(options, word=word)
+    return putkarma(options, word, value + change)
+
+
+def getkarma(options, word):
+    sql = "SELECT * FROM karma WHERE word='%s'" % word
+    cur.execute(sql)
+    value = cur.fetchone()
+    try:
+        # Get value from SQL query
+        value = value[1]
+    except:
+        createkarma(options, word)
+        value = 0
+    return value
+
+
+def createkarma(options, word):
+    sql = "INSERT INTO karma VALUES('%s',0)" % word
+    cur.execute(sql)
+    return con.commit()
+
+
+def createdb(options):
+    return cur.execute('CREATE TABLE karma(word TEXT, value INT)')
+
+
+def putkarma(options, word, value):
+    sql = "UPDATE karma SET value = '%s' WHERE word = '%s'" % (value, word)
+    cur.execute(sql)
+    con.commit()
+    return value
+
 # Main code
+
+
+# Initialize database access
+
+
+con = None
+try:
+    con = lite.connect(options.database)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM karma WHERE word='stampy'")
+    data = cur.fetchone()
+
+except lite.Error, e:
+    createdb(options)
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
+
+# Database initialized
+
 
 date = 0
 lastupdateid = 0
@@ -88,7 +151,8 @@ for message in getupdates(options):
     update_id = message['update_id']
     try:
         texto = message['message']['text']
-        chat = message['message']['chat']['id']
+        chat_id = message['message']['chat']['id']
+        message_id = int(message['message']['message_id'])
     except:
         error = 1
 
@@ -98,13 +162,29 @@ for message in getupdates(options):
     if newdate > date:
         date = newdate
         for word in texto.split():
-            if "++" in word:
-                print "++ Found in %s at %s with id %s" % (word, chat, update_id)
-            if "--" in word:
-                print "-- Found in %s at %s with id %s" % (word, chat, update_id)
+            if len(word) >= 4:
+                change = 0
+                if "++" == word[-2:]:
+                    print "++ Found in %s at %s with id %s" % (word, chat_id, message_id)
+                    change = 1
+                if "--" == word[-2:]:
+                    print "-- Found in %s at %s with id %s" % (word, chat_id, message_id)
+                    change = -1
+                if change != 0:
+                    # Remove last two characters from word (++ or --)
+                    word = word[0:-2]
+                    karma = updatekarma(options, word=word, change=change)
+                    if karma != 0:
+                        text = "%s now has %s karma points." % (word, karma)
+                    else:
+                        text = "%s now has no Karma and has been garbage collected." % word
+                    sendmessage(options, chat_id=chat_id, text=text, reply_to_message_id=message_id)
+    clearupdates(options, offset=update_id)
 
 print "Last processed message at %s" % date
 print "Last processed update id %s" % lastupdateid
 print "Last processed text %s" % texto
 
-# sendmessage(options, chat_id=options.control, text="Stampy execution ended")
+# Close database
+if con:
+    con.close()
