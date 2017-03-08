0aeb0571e109b0601084181bda0dd40fe9f3e207
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-03-08 21:18:24 +0100

    Add support to get El Jueves daily strip

---
+++ stampy/plugin/jueves.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Plugin for processing Jueves strip requests
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+
+import datetime
+import logging
+import dateutil.parser
+import feedparser
+from lxml import html
+from apscheduler.schedulers.background import BackgroundScheduler
+
+import stampy.stampy
+import stampy.plugin.stats
+import stampy.plugin.config
+
+sched = BackgroundScheduler()
+sched.start()
+
+
+def init():
+    """
+    Initializes module
+    :return:
+    """
+
+    sched.add_job(jueves, 'cron', id='jueves', hour='13', replace_existing=True,
+                  misfire_grace_time=120)
+
+    return
+
+
+def run(message):  # do not edit this line
+    """
+    Executes plugin
+    :param message: message to run against
+    :return:
+    """
+    text = stampy.stampy.getmsgdetail(message)["text"]
+    if text:
+        if text.split()[0].lower() == "/jueves":
+            juevescommands(message=message)
+        if stampy.plugin.config.config(key='owner') == stampy.stampy.getmsgdetail(message)["who_un"]:
+            if text.split()[0].lower() == "triggerjueves":
+                jueves()
+    return
+
+
+def help(message):  # do not edit this line
+    """
+    Returns help for plugin
+    :param message: message to process
+    :return: help text
+    """
+    commandtext = "Use `/jueves <date>` to get El Jueves comic "
+    commandtext += "strip for date or today (must be on RSS feed)\n\n"
+    return commandtext
+
+
+def juevescommands(message):
+    """
+    Searches for commands related to dilbert
+    :param message: message to process
+    :return:
+    """
+
+    logger = logging.getLogger(__name__)
+    msgdetail = stampy.stampy.getmsgdetail(message)
+
+    texto = msgdetail["text"]
+    chat_id = msgdetail["chat_id"]
+    message_id = msgdetail["message_id"]
+    who_un = msgdetail["who_un"]
+
+    logger.debug(msg="Command: %s by %s" % (texto, who_un))
+
+    # We might be have been given no command, just /dilbert
+    try:
+        date = texto.split(' ')[1]
+    except:
+        date = ""
+
+    try:
+        # Parse date or if in error, use today
+        date = dateutil.parser.parse(date)
+
+        # Force checking if valid date
+        day = date.day
+        month = date.month
+        year = date.year
+        date = datetime.datetime(year=year, month=month, day=day)
+    except:
+        date = datetime.datetime.now()
+
+    return jueves(chat_id=chat_id, date=date, reply_to_message_id=message_id)
+
+
+def jueves(chat_id=0, date=None, reply_to_message_id=""):
+    """
+    Sends El Jueves daily strip for the date provided
+    :param chat_id: chat to send image to
+    :param date: date to get the strip from that day
+    :param reply_to_message_id: Id of the message to send reply to
+    :return:
+    """
+
+    logger = logging.getLogger(__name__)
+
+    url = "http://www.eljueves.es/feeds/vineta-del-dia.html"
+
+    # Ping chat ID to not have chat removed
+    stampy.plugin.stats.pingchat(chat_id)
+
+    if not date:
+        date = datetime.datetime.now()
+
+    feed = feedparser.parse(url)
+    tira = []
+    for item in reversed(feed["items"]):
+        dateitem = dateutil.parser.parse(item["published"][:16])
+        if date.year == dateitem.year and date.month == dateitem.month and \
+           date.day == dateitem.day:
+            tira.append(item)
+
+    logger.debug(msg="# of Comics for today: %s" % len(tira))
+
+    for item in tira:
+        url = item['link']
+        tree = html.fromstring(item['summary'])
+        imgsrc = tree.xpath('//img/@src')[0]
+        imgtxt = item['title_detail']['value'] + "\n" + url + ""
+        stampy.stampy.sendimage(chat_id=chat_id, image=imgsrc, text=imgtxt, reply_to_message_id=reply_to_message_id)
+    return
