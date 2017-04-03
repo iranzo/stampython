db22354d788a9bf48e7e1865128e0714c5a7f11d
Javier Ramirez <javilinux@gmail.com>
2017-04-03 16:58:31 +0200

    Adding line after function description

--- stampy/plugin/stock.py
+++ stampy/plugin/stock.py
@@ -73,6 +73,7 @@ def get_currency_rate(currency, rate_in):
     :param rate_in: Destination Currency
     :return: rate
     """
+
     base_url = 'http://api.fixer.io/latest'
     query = base_url + '?base=%s&symbols=%s' % (currency, rate_in)
     try:
