096cfefd359e025c8304c4480c2d29b4c24f33dd
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-04-14 18:54:55 +0200

    Change hour for scheduled job

--- stampy/plugin/xkcd.py
+++ stampy/plugin/xkcd.py
@@ -27,7 +27,7 @@ def init():
     :return: List of triggers for plugin
     """
 
-    sched.add_job(xkcd, 'cron', id='xkcd', hour='17', replace_existing=True,
+    sched.add_job(xkcd, 'cron', id='xkcd', hour='12', replace_existing=True,
                   misfire_grace_time=120)
     triggers = ["^/xkcd"]
     return triggers
