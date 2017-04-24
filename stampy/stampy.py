e86d7178cb626d88103741fdf90be57069323312
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-04-24 18:08:33 +0200

    Fix error on if sentence with admin

--- stampy/stampy.py
+++ stampy/stampy.py
@@ -643,7 +643,7 @@ def is_owner_or_admin(message):
 
             # If we're not admin and admin is empty, consider ourselves admin
             if not admin:
-                if plugin.config.config(key='admin', gid=chat_id, default=True)
+                if plugin.config.config(key='admin', gid=chat_id, default=True):
                     admin = True
 
     return owner or admin
