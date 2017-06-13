b15a1cc826f860afd9c07fe9d76b1a34a40fadc2
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-06-13 23:49:43 +0200

    Add extra tests for karma module

--- tests/test_10_karma.py
+++ tests/test_10_karma.py
@@ -21,3 +21,51 @@ class TestStampy(TestCase):
     def test_updatekarmarem(self):
         stampy.plugin.karma.updatekarma('patata', -1)
         self.assertEqual(stampy.plugin.karma.getkarma('patata'), 1)
+
+    def test_rank(self):
+        messages = [{u'message': {u'date': 1478361249, u'text': u'rank',
+                                  u'from': {u'username': u'iranzo',
+                                            u'first_name': u'Pablo',
+                                            u'last_name': u'Iranzo G\xf3mez',
+                                            u'id': 5812695}, u'message_id': 108,
+                                  u'chat': {u'all_members_are_administrators': True,
+                                            u'type': u'group', u'id': -158164217,
+                                            u'title': u'BOTdevel'}},
+                     u'update_id': 837253571}]
+        stampy.stampy.process(messages)
+
+    def test_skarma(self):
+        messages = [{u'message': {u'date': 1478361249, u'text': u'skarma cebolla=20',
+                                  u'from': {u'username': u'iranzo',
+                                            u'first_name': u'Pablo',
+                                            u'last_name': u'Iranzo G\xf3mez',
+                                            u'id': 5812695}, u'message_id': 108,
+                                  u'chat': {u'all_members_are_administrators': True,
+                                            u'type': u'group', u'id': -158164217,
+                                            u'title': u'BOTdevel'}},
+                     u'update_id': 837253571}]
+        stampy.stampy.process(messages)
+
+    def test_srank(self):
+        messages = [{u'message': {u'date': 1478361249, u'text': u'srank iranzo',
+                                  u'from': {u'username': u'iranzo',
+                                            u'first_name': u'Pablo',
+                                            u'last_name': u'Iranzo G\xf3mez',
+                                            u'id': 5812695}, u'message_id': 108,
+                                  u'chat': {u'all_members_are_administrators': True,
+                                            u'type': u'group', u'id': -158164217,
+                                            u'title': u'BOTdevel'}},
+                     u'update_id': 837253571}]
+        stampy.stampy.process(messages)
+
+    def test_skarmapurge(self):
+        messages = [{u'message': {u'date': 1478361249, u'text': u'skarma purge',
+                                  u'from': {u'username': u'iranzo',
+                                            u'first_name': u'Pablo',
+                                            u'last_name': u'Iranzo G\xf3mez',
+                                            u'id': 5812695}, u'message_id': 108,
+                                  u'chat': {u'all_members_are_administrators': True,
+                                            u'type': u'group', u'id': -158164217,
+                                            u'title': u'BOTdevel'}},
+                     u'update_id': 837253571}]
+        stampy.stampy.process(messages)
