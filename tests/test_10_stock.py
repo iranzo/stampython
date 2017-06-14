ba9303b2a10145a61f1cfdd1bf98c52fff7c5bbb
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-06-14 23:34:14 +0200

    Validate also other stocks

--- tests/test_10_stock.py
+++ tests/test_10_stock.py
@@ -26,3 +26,15 @@ class TestStampy(TestCase):
                                             u'title': u'BOTdevel'}},
                      u'update_id': 837253571}]
         stampy.stampy.process(messages)
+
+    def test_stocktickers(self):
+        messages = [{u'message': {u'date': 1478361249, u'text': u'stock RHT MSFT',
+                                  u'from': {u'username': u'iranzo',
+                                            u'first_name': u'Pablo',
+                                            u'last_name': u'Iranzo G\xf3mez',
+                                            u'id': 5812695}, u'message_id': 108,
+                                  u'chat': {u'all_members_are_administrators': True,
+                                            u'type': u'group', u'id': -158164217,
+                                            u'title': u'BOTdevel'}},
+                     u'update_id': 837253571}]
+        stampy.stampy.process(messages)
