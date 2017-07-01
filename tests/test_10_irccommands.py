84dea8c639d26001bf3390491833d7a32c01f834
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-07-01 22:09:47 +0200

    Add test for unban

--- tests/test_10_irccommands.py
+++ tests/test_10_irccommands.py
@@ -176,3 +176,21 @@ class TestStampy(TestCase):
                                                       "date": 1497476576, "text": "test"}, "text": "/op"}}]
 
         stampy.stampy.process(messages)
+
+    def test_unban(self):
+        true = True
+        messages = [{"update_id": 240168933,
+                     "message": {"message_id": 4831,
+                                 "from": {"id": 5812695, "first_name": "Pablo", "last_name": "Iranzo G\u00f3mez",
+                                          "username": "iranzo", "language_code": "es"},
+                                 "chat": {"id": -158164217, "title": "BOTdevel", "type": "group",
+                                          "all_members_are_administrators": true}, "date": 1497476585,
+                                 "reply_to_message": {"message_id": 4830, "from": {"id": 5812695, "first_name": "Pablo",
+                                                                                   "last_name": "Iranzo G\u00f3mez",
+                                                                                   "username": "iranzo",
+                                                                                   "language_code": "es"},
+                                                      "chat": {"id": -158164217, "title": "BOTdevel", "type": "group",
+                                                               "all_members_are_administrators": true},
+                                                      "date": 1497476576, "text": "test"}, "text": "/unban"}}]
+
+        stampy.stampy.process(messages)
