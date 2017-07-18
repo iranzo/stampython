535252f13655b779798dfdc5fc91a7b69578184f
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-07-18 22:08:39 +0200

    Allow to send messages from CLI using bot settings in default database

---
+++ sendmessage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Sends a message to a chatID via CLI
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+
+
+import argparse
+import sys
+from stampy.stampy import sendmessage
+
+
+description = 'Sends a message using bot to chatid'
+
+# Option parsing
+p = argparse.ArgumentParser("sendmessage.py [arguments]", description=description)
+p.add_argument("-i", "--id", dest="chat_id", help="chat_id to send message to", default=False)
+p.add_argument("-t", "--text", dest="texto", help="Text to send", default=False)
+
+options, unknown = p.parse_known_args()
+
+if options.texto and options.chat_id:
+    sendmessage(chat_id=options.chat_id, text=options.texto)
